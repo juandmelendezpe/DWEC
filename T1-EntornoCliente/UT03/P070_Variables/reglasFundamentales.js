f915265446350c325e4e1db78d0382b7222484f5
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-09-24 14:40:37 +0200

    update files proyect
    update files

---
+++ T1-EntornoCliente/UT03/P070_Variables/reglasFundamentales.js
@@ -0,0 +1,13 @@
+function reglas(){
+    alert("reglas fundamentales de java script");
+    var x=8;
+    console.log(" la variable es : " + x);
+}
+window.onload = function() {
+
+    document.getElementById("botonReglas").addEventListener("click",reglas);
+     {
+        
+       
+    }
+}
