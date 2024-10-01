c48e107349d9a3ab552bbffd96435d73ae69fe3f
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-10-01 14:25:25 +0200

    update files
    update files - arrays

--- T1-EntornoCliente/UT04_EstructurasDeControl/cuadro.js
+++ T1-EntornoCliente/UT04_EstructurasDeControl/cuadro.js
@@ -1,20 +1,11 @@
-function rellenarCubo(lado) {
-    // Crear un conjunto para almacenar los valores únicos
-    const valoresUnicos = new Set();
+// asignar valor a un div
+let numero = 9;
+document.getElementById("resultado").textContent = numero;
 
-    // Función para generar un valor aleatorio único
-    function generarValorUnico() {
-        let valor;
-        do {
-            valor = Math.floor(Math.random() * 9) + 1;
-        } while (valoresUnicos.has(valor));
-        valoresUnicos.add(valor);
-        return valor;
-    }
-}
-window.onload = function() {
-    document.getElementById("botonRellenar").addEventListener("click", function() {
-        const lado = document.getElementById("lado").value;
-        rellenarCubo(lado);
-    });
-}
+let valorDiv;
+valorDiv = document.getElementById("resultado").textContent;
+alert("el valor del div es : " + valorDiv);
+
+window.onload = function () {
+  document.getElementById("botonRellenar").addEventListener("click", rellenarCubo);
+};
