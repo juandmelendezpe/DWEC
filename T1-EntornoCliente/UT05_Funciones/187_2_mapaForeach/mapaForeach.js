6e56364758b4e23428f8523a57fbf9c2c0787e7d
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-10-29 00:23:17 +0100

    update play simon
    update simon play , ejercicio 2

--- T1-EntornoCliente/UT05_Funciones/187_2_mapaForeach/mapaForeach.js
+++ T1-EntornoCliente/UT05_Funciones/187_2_mapaForeach/mapaForeach.js
@@ -1,8 +1,9 @@
+
 function mapaForeach() {
   const provincias = new Map();
-  provincias.set(l, "Álava").set(28, "Madrid").set(34, "Palencia").set(41, "Sevilla");
-  provincias.forEach(function () {
-    consolé.log("Clave: ${clave}, Valor: ${valor}");
+  provincias.set(1, "Álava").set(28, "Madrid").set(34, "Palencia").set(41, "Sevilla");
+  provincias.forEach(function (clave, valor) {
+    console.log(`Clave: ${clave}, Valor: ${valor}`);
   });
 }
 window.onload = function() {
--- T1-EntornoCliente/UT06_Objetos_js/208_1_Objeto_Comillas/objetoComillas.js
+++ T1-EntornoCliente/UT06_Objetos_js/208_1_Objeto_Comillas/objetoComillas.js
@@ -1,23 +1,29 @@
-let libro = {
-    título : "Manual de UFOlogía",
-    autores : [ "Pedro Martínez","Ana León"],
-    //Esta propiedad es un array
-    editorial : { //La editorial es otro objeto
-                nombre : "Inexistente S.A.",
-                país : "España"
-    },
-    ArrayUno:['uno','Dos'],
-    ArrayDos:new Array('hola','hola'),
-    map : new Map([1,"Jose"],[2,"Maria"],[3,"Elena"]),
-    set : new Set([1,2,3,4,5,6,7])
+function crearObjetoEntreComillas() {
+    let libro = {
+        titulo: "Manual de UFOlogía",
+        autores: ["Pedro Martínez", "Ana León"], // Esta propiedad es un array
+        editorial: { // La editorial es otro objeto
+            nombre: "Inexistente S.A.",
+            pais: "España"
+        },
+        arrayUno: ['uno', 'dos'],
+        arrayDos: ['hola', 'hola'],
+        map: new Map([
+            [1, "Jose"],
+            [2, "Maria"],
+            [3, "Elena"]
+        ]),
+        set: new Set([1, 2, 3, 4, 5, 6, 7])
     };
-    console.log(libro.título);
-    console.log(libro['n-serie']);
+    console.log(libro.titulo);
     console.log(libro.editorial.nombre);
-    console.log(libro.ArrayUno);
-    console.log(libro.ArrayDos);
+    console.log(libro.arrayUno);
+    console.log(libro.arrayDos);
+}
+window.onload = function() {
+    document.getElementById("objetoComillas").addEventListener("click", crearObjetoEntreComillas);
+}
 
-    window.onload = function(){
-        document.getElementById("objetoComillas").addEventListener("click",crearObjetoEnterCominllas);
-    }
+
+
 
