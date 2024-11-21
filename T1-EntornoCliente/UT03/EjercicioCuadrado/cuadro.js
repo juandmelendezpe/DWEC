07bd478c4d4a82d752bfeb5b22fc6c7fb7e061ab
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-11-21 07:47:14 +0100

    tarea 1
    tarea 1

--- T1-EntornoCliente/UT03/EjercicioCuadrado/cuadro.js
+++ T1-EntornoCliente/UT03/EjercicioCuadrado/cuadro.js
@@ -1,10 +1,12 @@
 // asignar valor a un div
+function rellenarCubo() {
 let numero = 9;
 document.getElementById("resultado").textContent = numero;
 
 let valorDiv;
 valorDiv = document.getElementById("resultado").textContent;
 alert("el valor del div es : " + valorDiv);
+}
 
 window.onload = function () {
   document.getElementById("botonRellenar").addEventListener("click", rellenarCubo);
