39656134e56438d90ea0cab6aa258da4574213f6
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2025-02-26 10:46:23 +0100

    update files
    update files

--- EjerciciosPracticosRepaso/RepasoExamen_v1_figuras/figuras.js
+++ EjerciciosPracticosRepaso/RepasoExamen_v1_figuras/figuras.js
@@ -2,6 +2,7 @@ let figuras = ["circuloAzul", "circuloRojo", "circuloVerde",
     "cuadradoAzul", "cuadradoRojo", "cuadradoVerde",
     "trianguloAzul", "trianguloRojo", "trianguloVerde"
 ];
+let posicionActual = 0;
 function devolverNumero(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 }
@@ -34,7 +35,37 @@ function cargarImagenesCentral(){
     }
 }
 
+
+function ocultarFigura(elemento){
+    let valorElemento = elemento.target.id;
+    //alert("has pulsado el elemento :" +valorElemento);
+    var cont = 0 ;
+    var temp = setInterval(
+        function(){
+            document.getElementById(valorElemento).classList.toggle("ocultar");
+            comparaFiguras(elemento);
+            cont++;
+            console.log("hola" + cont);
+            if(cont >=4){
+                clearInterval(temp)
+            }
+        },750);
+}
+function comparaFiguras(idElemento){
+    let valorElemento = idElemento.target.id;
+    if(valorElemento == posicionActual){
+        alert("los elementos son iguales :" +valorElemento +"=" + posicionActual);
+    }else{
+        alert("son distintos");
+    }
+}
 window.onload = function() {
     cargarSecuencia();
     cargarImagenesCentral();
+    for(i =0 ; 0 < 6 ; i++){
+        let idposicion= "posicion"+i;
+        document.getElementById(idposicion).addEventListener("click",ocultarFigura);
+
+    }
+
 }
