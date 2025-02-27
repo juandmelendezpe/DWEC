2dc13da39a25392c3f1644414fca6f40abdf9836
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2025-02-27 09:54:45 +0100

    update new chances
    new chances

--- EjerciciosPracticosRepaso/RepasoExamen_v1_figuras/figuras.js
+++ EjerciciosPracticosRepaso/RepasoExamen_v1_figuras/figuras.js
@@ -31,30 +31,32 @@ function cargarImagenesCentral(){
             document.getElementById(posicionTablero).setAttribute("src",nombreImagenTablero);
             completado++;
         }
-
     }
 }
-
-
 function ocultarFigura(elemento){
     let valorElemento = elemento.target.id;
     //alert("has pulsado el elemento :" +valorElemento);
     var cont = 0 ;
     var temp = setInterval(
         function(){
-            document.getElementById(valorElemento).classList.toggle("ocultar");
-            comparaFiguras(elemento);
-            cont++;
-            console.log("hola" + cont);
-            if(cont >=4){
+            if(cont >=6){
                 clearInterval(temp)
+            }else{
+                document.getElementById(valorElemento).classList.toggle("ocultar");
+                cont++;
             }
         },750);
 }
-function comparaFiguras(idElemento){
-    let valorElemento = idElemento.target.id;
-    if(valorElemento == posicionActual){
-        alert("los elementos son iguales :" +valorElemento +"=" + posicionActual);
+function comparaFiguras(idPosicion){
+    let valorPosicion = idPosicion.target.id;//valorElemento = "posicion0"
+    let posicionElemento = document.getElementById(valorPosicion).getAttribute("src");
+    let idPosicion = "imagenAbajo"+ posicionActual;
+    let posicionSecuencia = document.getElementById(idPosicion).getAttribute("src");
+    if(posicionElemento == posicionSecuencia){
+        alert("los elementos son iguales :" +posicionElemento + " = " + posicionSecuencia);
+        //ocultarFigura(idElemento);
+        ocultarFigura(idPosicion);
+        //document.getElementById(idPosicion).classList.toggle("ocultar");
     }else{
         alert("son distintos");
     }
@@ -64,8 +66,6 @@ window.onload = function() {
     cargarImagenesCentral();
     for(i =0 ; 0 < 6 ; i++){
         let idposicion= "posicion"+i;
-        document.getElementById(idposicion).addEventListener("click",ocultarFigura);
-
+        document.getElementById(idposicion).addEventListener("click",comparaFiguras);
     }
-
 }
