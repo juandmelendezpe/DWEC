68b3557c5be11aacc77229e53153507149de5f5b
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2025-03-18 13:46:38 +0100

    update files
    update files

--- EjerciciosPracticosRepaso/Repaso_01_HundirFlota/script.js
+++ EjerciciosPracticosRepaso/Repaso_01_HundirFlota/script.js
@@ -1,7 +1,8 @@
-let barcos = ["barco1", "barco2","barco3","barco4"];
+let barcos = ["barco0", "barco1","barco2","barco3"];
 let agua = "agua";
 let estrella = "estrella";
 let estrellaBrilla = "estrellaBrilla";
+let puntos = 3;
 
 function devolverNumero(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
@@ -9,19 +10,26 @@ function devolverNumero(minimo, maximo) {
 function cargarBarcos(){
     let barcosUsados = new Set();
     let completado = 0;
+    let estrellasColocadas = 0;
     while(completado <=11){
-        let numPosicion = devolverNumero(1,3);
-        let posicionImagen = "imagen" + completado;
+        let numPosicion = devolverNumero(0,3);
+        let posicionImagen = "imagen" + numPosicion;
         let nombreImagenBarco = "./imagenes/"+barcos[numPosicion]+".png";
-        //let nombreImagenAgua = "./imagenes/"+agua+".png";
-        //let nombreImagenEstrella = "./imagenes/"+estrella+".png";
+        let nombreImagenAgua = "./imagenes/"+agua+".png";
+        let nombreImagenEstrella = "./imagenes/"+estrella+".png";
 
         if(!barcosUsados.has(nombreImagenBarco)){
             document.getElementById(posicionImagen).setAttribute("src", nombreImagenBarco);
             barcosUsados.add(barcos[numPosicion]);
             completado++;
+            
+          
+        /* if(document.getElementById(posicionImagen).getAttribute("src")==""){
+                document.getElementById(posicionImagen).setAttribute("src",nombreImagenEstrella);
+                estrellasColocadas++; 
+            }
+                */
         }
-
     }
 }
 function cargarAgua() {
@@ -31,14 +39,19 @@ function cargarAgua() {
         recuperacionFiguras[i].setAttribute("src", nombreImagenAgua); // Cambiamos la imagen por agua
     }
 }
+function ocultarBarcos(){
+   let ocultar = document.getElementsByClassName("imagen");
+    ocultar.classList.add("oculto");
+}
 
 
 
 function ocultarPolicia() {
     let policia = document.getElementById("policia");
     alert("el policia se ocultara y el valor id es: " + policia.id);
-    policia.classList.add("oculto");
-    cargarAgua();
+    //policia.classList.add("oculto");
+    policia.setAttribute("src","./imagenes/ocultar.png")
+    //ocultarBarcos();
 
 }
 window.onload = function() {
