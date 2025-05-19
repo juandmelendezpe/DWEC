345bae1aba0daac7e0f3186926ea42a8fe6b6a73
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2025-05-19 11:46:35 +0200

    update Solu Recu final Examen
    Ejercicio de HUndir  la flota con  Puntuacion.

--- Examen2_final_DWEC/examen2do.js
+++ Examen2_final_DWEC/examen2do.js
@@ -1,30 +1,26 @@
-let figurasArray = ["circuloAzul","circuloRojo","circuloVerde",
+figurasArray = ["circuloAzul","circuloRojo","circuloVerde",
 "cuadradoAzul","cuadradoRojo","cuadradoVerde",
-"trianguloAzul","trianguloRojo","trianguloVerde",
-];
+"trianguloAzul","trianguloRojo","trianguloVerde"];
 figurasusadas = new Set();
 
-let puntuacion = 0;
-let vidas = 0;
+ puntuacion = 0;
+vidas = 0;
 
 function devolverNumero(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 }
-function compararFiguras(elemento){
-    let idtablero = elemento.target.id;
-    alert("Has pulsado en el tablero "+idtablero);
-
-}
 function cargarSecuencia(){
     let completado = 0;
+        figurasUsadas = new Set();
+
     while(completado < 6){
         let numeroPosicion = devolverNumero(0,8);
         let posicionSecuencia = "secuencia"+completado;
-        let nombreImagen = "./imagenes/"+figura[numeroPosicion]+".png";
+        let nombreImagen = "./imagenes/"+figurasArray[numeroPosicion]+".png";
 
-        if(!figurasusadas.has(figuras[numeroPosicion])){
+        if(!figurasUsadas.has(figurasArray[numeroPosicion])){
             document.getElementById(posicionSecuencia).setAttribute("src",nombreImagen);
-            document.getElementById(posicionSecuencia).setAttribute("alt",figuras[numeroPosicion]);
+            figurasUsadas.add(figurasArray[numeroPosicion]);
             completado++;
     }
 }
@@ -36,14 +32,21 @@ function cargarTablero(){
     while(completado < 6){
         let numeroPosicion = devolverNumero(0,8);
         let posicionTablero = "juego"+completado;
-        let nombreImagen = "./imagenes/"+figura[numeroPosicion]+".png";
+        let nombreImagen = "./imagenes/"+figurasArray[numeroPosicion]+".png";
 
-        if(!figurasusadas.has(figuras[numeroPosicion])){
+        if(!figurasusadas.has(figurasArray[numeroPosicion])){
             document.getElementById(posicionTablero).setAttribute("src",nombreImagen);
-            document.getElementById(posicionTablero).setAttribute("alt",figuras[numeroPosicion]);
+            document.getElementById(posicionTablero).setAttribute("alt",figurasArray[numeroPosicion]);
+            figurasusadas.add(figurasArray[numeroPosicion]);
+            
             completado++;
         }
 }
+}
+function compararFiguras(elemento){
+    let idtablero = elemento.target.id;
+    alert("Has pulsado en el tablero "+idtablero);
+
 }
 function borrarVidas(){
     let vidas = 3;
@@ -57,4 +60,7 @@ window.onload = function() {
         let idSecuencia = "secuencia"+i;
         document.getElementById(idSecuencia).addEventListener("click",compararFiguras);
     }
+    cargarSecuencia();
+    cargarTablero();
+    borrarVidas();
 }
--- T1-EntornoCliente/UT7_ObjetoWindow_DOM/01_evaluacion_examen/ordenarImagenes.js
+++ T1-EntornoCliente/UT7_ObjetoWindow_DOM/01_evaluacion_examen/ordenarImagenes.js
@@ -6,24 +6,64 @@ let imagenDos = "";
 function devolverNumero(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
-function comprobarColocadas(){
+function cargarImagenes() { // carga las imagenes en el tablero
+    let numImagen = devolverNumero(1,3); // devuelve un numero entre 1 y 3
+    let imagenes = document.getElementsByClassName("imagen"); /*  la variable imagenes recoge todos los elementos con la clase imagen  /*/
 
-    let colocadas = true;
-    for(let i = 1; i<=9; i++) {
-        let alt = document.getElementById("imagen"+i).getAttribute("alt");
-        let src = document.getElementById("imagen"+i).getAttribute("src");
-        if(alt != src) {
-            colocadas = false;
-        }
-    }
-    if(colocadas) {
-        alert("Has ganado");
-        let imagenes = document.getElementsByClassName("imagen");
-        for(let i = 1; i<=imagenes.length; i++) {
-            document.getElementById("imagen"+i).classList.add("completado");
-        }
+    for(let i = 1; i <= imagenes.length; i++) {
+        let valorImagen = "./imagenes/" + tablero[numImagen-1] + "_" + i + ".png";// iteramos el array tablero y le añadimos el numero de la imagen
+
+        document.getElementById("imagen"+i).setAttribute("src", valorImagen); // cambiamos el atributo src de la imagen
+        document.getElementById("imagen"+i).setAttribute("alt", valorImagen); // cambiamos el atributo alt de la imagen
     }
 }
+function descolocarImagenes() { // descoloca las imagenes
+    // iteramos 100 veces para descolocar las imagenes
+    // cogemos dos imagenes aleatorias y cambiamos su atributo src
+    for(let i=0; i <= 100; i++) {
+        let imagenUnoDes = "imagen" + devolverNumero(1,9);
+        let imagenDosDes = "imagen" + devolverNumero(1,9);
+        let valorImagenUno = document.getElementById(imagenUnoDes).getAttribute("src");
+        let valorImagenDos = document.getElementById(imagenDosDes).getAttribute("src");
+        document.getElementById(imagenUnoDes).setAttribute("src",valorImagenDos);
+        document.getElementById(imagenDosDes).setAttribute("src",valorImagenUno);
+    }
+}
+
+function cambiarImagenes(elemento) {
+   // let valorRecuperado = elemento.target.id;
+   // alert("valor Pulsado : "+ valorRecuperado);
+   if(imagenUno == "") {
+         imagenUno = elemento.target.id;
+    } else {
+        imagenDos = elemento.target.id;
+        let rutaImagenUno = document.getElementById(imagenUno).getAttribute("src"); // recuperamos la ruta de la imagen uno
+        let rutaImagenDos = document.getElementById(imagenDos).getAttribute("src");
+
+        document.getElementById(imagenUno).setAttribute("src",rutaImagenDos);
+        document.getElementById(imagenDos).setAttribute("src",rutaImagenUno);
+
+        comprobarVidas(imagenUno,imagenDos);
+
+        imagenUno = "";
+        imagenDos = "";
+
+        comprobarColocadas();
+    }
+
+}
+/*
+ en esta funcion comprobamos si las imagenes son iguales o no
+ si son iguales no hacemos nada
+    si no son iguales cambiamos la imagen de la dino
+    si la dino es la cuarta imagen cambiamos todas las imagenes a su estado inicial
+    y cambiamos la imagen de la dino a su estado inicial
+    si la dino es la primera imagen cambiamos la imagen de la dino a su estado inicial
+    si la dino es la segunda imagen cambiamos la imagen de la dino a su estado inicial
+    si la dino es la tercera imagen cambiamos la imagen de la dino a su estado inicial
+    si la dino es la cuarta imagen cambiamos la imagen de la dino a su estado inicial
+
+*/
 function comprobarVidas(imagenUnoLocal, imagenDosLocal) {
     let altUno = document.getElementById(imagenUnoLocal).getAttribute("alt");
     let srcUno = document.getElementById(imagenUnoLocal).getAttribute("src");
@@ -45,47 +85,22 @@ function comprobarVidas(imagenUnoLocal, imagenDosLocal) {
         vidas = 1;
     }
 }
-function cambiarImagenes(elemento) {
-   // let valorRecuperado = elemento.target.id;
-   // alert("valor Pulsado : "+ valorRecuperado);
-   if(imagenUno == "") {
-         imagenUno = elemento.target.id;
-    } else {
-        imagenDos = elemento.target.id;
-        let rutaImagenUno = document.getElementById(imagenUno).getAttribute("src");
-        let rutaImagenDos = document.getElementById(imagenDos).getAttribute("src");
+function comprobarColocadas(){
 
-        document.getElementById(imagenUno).setAttribute("src",rutaImagenDos);
-        document.getElementById(imagenDos).setAttribute("src",rutaImagenUno);
-
-        comprobarVidas(imagenUno,imagenDos);
-
-        imagenUno = "";
-        imagenDos = "";
-
-        comprobarColocadas();
+    let colocadas = true;
+    for(let i = 1; i<=9; i++) {
+        let alt = document.getElementById("imagen"+i).getAttribute("alt");
+        let src = document.getElementById("imagen"+i).getAttribute("src");
+        if(alt != src) {
+            colocadas = false;
+        }
     }
-
-}
-function cargarImagenes() {
-    let numImagen = devolverNumero(1,3);
-    let imagenes = document.getElementsByClassName("imagen");
-
-    for(let i = 1; i <= imagenes.length; i++) {
-        let valorImagen = "./imagenes/" + tablero[numImagen-1] + "_" + i + ".png";
-
-        document.getElementById("imagen"+i).setAttribute("src", valorImagen);
-        document.getElementById("imagen"+i).setAttribute("alt", valorImagen);
-    }
-}
-function descolocarImagenes() {
-    for(let i=0; i <= 100; i++) {
-        let imagenUnoDes = "imagen" + devolverNumero(1,9);
-        let imagenDosDes = "imagen" + devolverNumero(1,9);
-        let valorImagenUno = document.getElementById(imagenUnoDes).getAttribute("src");
-        let valorImagenDos = document.getElementById(imagenDosDes).getAttribute("src");
-        document.getElementById(imagenUnoDes).setAttribute("src",valorImagenDos);
-        document.getElementById(imagenDosDes).setAttribute("src",valorImagenUno);
+    if(colocadas) {
+        alert("Has ganado");
+        let imagenes = document.getElementsByClassName("imagen");
+        for(let i = 1; i<=imagenes.length; i++) {
+            document.getElementById("imagen"+i).classList.add("completado");
+        }
     }
 }
 window.onload = function() {
